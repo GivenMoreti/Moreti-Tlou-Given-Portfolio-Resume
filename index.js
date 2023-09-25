9143c1d6b77d4a2f7b30f08328bd82cf84b7cb58
Given <moretigiven@gmail.com>
2023-09-25 15:32:57 +0200

    scroll up button

--- index.js
+++ index.js
@@ -16,3 +16,35 @@ const inputs = document.querySelectorAll("input");
         alert("Some fields are empty!");
       }
     });
+
+    // scroll up button
+    const scrollUpBtn = document.getElementById("scroll-up");
+    scrollUpBtn.addEventListener("click",()=>{
+      window.scrollTo({
+        top:0,
+        left:0,
+        behavior:"smooth",
+      
+      })  
+      
+    })
+
+    //typing effect
+   
+    // let charIndex = 0;
+    // const typingText = document.querySelector(".typing-text");
+    
+    // function type() {
+    //     if (charIndex < text.length) {
+    //         typingText.textContent += text.charAt(charIndex);
+    //         charIndex++;
+    //         setTimeout(type, 100000); // Adjust typing speed here (milliseconds)
+    //     } else {
+    //         // Stop the animation when the text is fully typed
+    //         clearTimeout(typingTimeout);
+    //     }
+    // }
+    
+    // // Start the typing animation
+    // const typingTimeout = setTimeout(type, 100000);
+    
